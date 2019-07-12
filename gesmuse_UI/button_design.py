b6ac762070a971b5fe02d01ffa34b8fd61a1e242
ZhaoRibo <1294396795@qq.com>
2019-07-12 15:50:25 +0800

    Try to save this storage
    增加gitignore，避免.idea和venv中的文件上传
    以及部分代码的小改动
    将部分文件夹改为了python package

--- gesmuse_UI/button_design.py
+++ gesmuse_UI/button_design.py
@@ -1,66 +1,53 @@
+"""
+调用示例：
+from gesmuse_UI.button_design import Button
+startbutton = Button.StartButton(self)
+"""
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QPushButton
 
 
-class Button(QWidget):
+class Button:
 
     def __init__(self):
-        super().__init__()
+        pass
 
-        self.initUI()  # 界面绘制交给InitUi方法
-
-    def initUI(self):
-        # 控件QPushButton的定义和设置
-        startButton = QPushButton(self)
+    def StartButton(self):
+        startButton = QPushButton()
         startButton.setMinimumSize(50, 100)  # 设置最小大小，主页面按钮图标原尺寸为200:400，为使图标不变形，请保持比例不变
         startButton.setStyleSheet("QPushButton{border-image: url(../gesmuse_resources/image/start_img-1.png)}"
                                   "QPushButton:hover{border-image: url(../gesmuse_resources/image/start_img-2.png)}"
                                   "QPushButton:pressed{border-image: url(../gesmuse_resources/image/start_img-1.png)}")
-        opusButton = QPushButton(self)
+        return startButton
+
+    def OpusButton(self):
+        opusButton = QPushButton()
         opusButton.setMinimumSize(50, 100)
         opusButton.setStyleSheet("QPushButton{border-image: url(../gesmuse_resources/image/opus_img-1.png)}"
                                  "QPushButton:hover{border-image: url(../gesmuse_resources/image/opus_img-2.png)}"
                                  "QPushButton:pressed{border-image: url(../gesmuse_resources/image/opus_img-1.png)}")
+        return opusButton
 
+    def SettingButton(self):
         settingButton = QPushButton()
         settingButton.setMinimumSize(50, 100)
         settingButton.setStyleSheet("QPushButton{border-image: url(../gesmuse_resources/image/setting_img-1.png)}"
                                     "QPushButton:hover{border-image: url(../gesmuse_resources/image/setting_img-2.png)}"
                                     "QPushButton:pressed{border-image: url(../gesmuse_resources/image/setting_img-1.png)}")
+        return settingButton
 
+    def HelpButton(self):
         helpButton = QPushButton()
         helpButton.setMinimumSize(50, 100)
         helpButton.setStyleSheet("QPushButton{border-image: url(../gesmuse_resources/image/help_img-1.png)}"
                                  "QPushButton:hover{border-image: url(../gesmuse_resources/image/help_img-2.png)}"
                                  "QPushButton:pressed{border-image: url(../gesmuse_resources/image/help_img-1.png)}")
+        return helpButton
 
+    def ExitButton(self):
         exitButton = QPushButton()
         exitButton.setMinimumSize(50, 100)
         exitButton.setStyleSheet("QPushButton{border-image: url(../gesmuse_resources/image/exit_img-1.png)}"
                                  "QPushButton:hover{border-image: url(../gesmuse_resources/image/exit_img-2.png)}"
                                  "QPushButton:pressed{border-image: url(../gesmuse_resources/image/exit_img-1.png)}")
-
-        # 布局
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(startButton)
-        hbox.addWidget(opusButton)
-        hbox.addWidget(settingButton)
-        hbox.addWidget(helpButton)
-        hbox.addWidget(exitButton)
-        hbox.addStretch(1)
-
-        # 设置布局
-        self.setLayout(hbox)
-        # 设置窗口的位置和大小
-        self.setGeometry(300, 300, 1000, 800)
-        # 设置窗口的标题
-        self.setWindowTitle('Gesmuse_Button')
-        self.show()
-
-
-if __name__ == '__main__':
-    # 创建应用程序和对象
-    app = QApplication(sys.argv)
-    ex = Button()
-    sys.exit(app.exec_())
+        return exitButton
